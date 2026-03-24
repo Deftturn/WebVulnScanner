6232f94794efc6e5eeb9cb850dbb1c6524bb380c
Defturn <whizzk307@gmail.com>
2026-03-24 22:52:19 +0000

    Added docker

---
+++ scanner/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Web Vulnerability Scanner
+Main entry point for the application
+"""
+
+print("Web Vulnerability Scanner starting...")
+
+# TODO: Implement scanner logic
+# For now, just keep the container running
+import time
+while True:
+    time.sleep(1)
